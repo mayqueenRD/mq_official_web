672f9a230693d2459433f6bc1f60bd6210cdfc59
Wig C <wigcheng@tailyn.com.tw>
2014-12-19 13:56:09 +0800

    Linking from frontend to backend.

--- main.py
+++ main.py
@@ -7,5 +7,18 @@ app = Flask(__name__)
 def main():
     return render_template('mayqueen_web/index.html')
 
+@app.route("/team")
+def mobile():
+    return render_template('mayqueen_web/team.html')
+
+@app.route("/news")
+def news():
+    return render_template('mayqueen_web/news.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('mayqueen_web/contact1.html')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
