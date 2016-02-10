4b8000d149d2a4bf4773918d7393beafc5b4fea5
henrybi <henrybi@u.washington.edu>
2016-02-10 14:21:28 -0800

    clean up html code

--- captcha.php
+++ captcha.php
@@ -1,19 +1,19 @@
-<?php 
-session_start(); 
+<?php
+session_start();
 
 $alphanum = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
 
 // generate the verication code
-$rand = substr(str_shuffle($alphanum), 0, 5); 
-$_SESSION["vercode"] = $rand; 
-$height = 25; 
-$width = 65; 
- 
+$rand = substr(str_shuffle($alphanum), 0, 5);
+$_SESSION["vercode"] = $rand;
+$height = 25;
+$width = 65;
+
 $image_p = imagecreate($width, $height); 
-$black = imagecolorallocate($image_p, 0, 0, 0); 
-$white = imagecolorallocate($image_p, 255, 255, 255); 
-$font_size = 14; 
- 
-imagestring($image_p, $font_size, 5, 5, $rand, $white); 
-imagejpeg($image_p, null, 80); 
+$black = imagecolorallocate($image_p, 0, 0, 0);
+$white = imagecolorallocate($image_p, 255, 255, 255);
+$font_size = 14;
+
+imagestring($image_p, $font_size, 5, 5, $rand, $white);
+imagejpeg($image_p, null, 80);
 ?>
