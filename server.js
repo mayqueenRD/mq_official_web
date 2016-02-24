85d21a2130c14f8b0f0737f3a955211f67cc7a60
henrybi <henrybi@u.washington.edu>
2016-02-24 08:18:28 -0800

    updated node server running

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+var http = require('http');
+
+//Lets define a port we want to listen to
+var PORT=3000;
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
 var nodemailer = require('nodemailer');
 
 // create reusable transporter object using the default SMTP transport
@@ -7,9 +20,9 @@ var xoauth2 = require('xoauth2');
 
 // listen for token updates (if refreshToken is set)
 // you probably want to store these to a db
-generator.on('token', function(token){
-    console.log('New token for %s: %s', token.user, token.accessToken);
-});
+// generator.on('token', function(token){
+//     console.log('New token for %s: %s', token.user, token.accessToken);
+// });
 
 // login
 var transporter = nodemailer.createTransport({
@@ -40,4 +53,10 @@ transporter.sendMail(mailOptions, function(error, info){
         return console.log(error);
     }
     console.log('Message sent: ' + info.response);
+});
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
 });
