4b82d68e4932b91ddd46384c9d15242694dd03f6
henrybi <henrybi@u.washington.edu>
2016-02-25 17:12:05 -0800

    updated contact page

--- server.js
+++ server.js
@@ -1,43 +1,34 @@
 var express = require('express');
 var nodemailer = require("nodemailer");
-var smtpTransport = require("nodemailer-smtp-transport");
 var app = express();
 
-var smtpTransport = nodemailer.createTransport(smtpTransport({
-    host : "smtp.gmail.com",
-    auth : {
-        user : "henrybidesign@gmail.com",
-        pass : "Orange999999999"
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    service: "Gmail",
+    auth: {
+        user: "henrybidesign@gmail.com",
+        pass: "Orange999999999"
     }
-}));
-app.get('/send',function(req,res){
-    var mailOptions={
-        from : "henrybidesign@gmail.com",
-        to : "henrybi@uw.edu",
-        subject : "Node Mailer",
-        text : "Your Text",
-        html : "HTML GENERATED",
-        attachments : [
-            {   // file on disk as an attachment
-                filename: 'text3.txt',
-                path: 'Your File path' // stream this file
-            }
-        ]
-    };
-    console.log(mailOptions);
-    smtpTransport.sendMail(mailOptions, function(error, response){
-        if(error){
-            console.log(error);
-            res.end("error");
-        }else{
-            console.log(response.response.toString());
-            console.log("Message sent: " + response.message);
-            res.end("sent");
-        }
-    });
 });
 
+app.get('/send',function(req,res){
+  var mailOptions = {
+    to : "henrybi@uw.edu",
+    subject : "test",
+    text : "test"
+  };
+  console.log(mailOptions);
 
-app.listen(3000,function(){
-    console.log("Express Started on Port 3000");
+  smtpTransport.sendMail(mailOptions, function(error, response){
+    if(error){
+    console.log(error);
+    res.end("error");
+    }else{
+    console.log("Message sent: " + response.message);
+    res.end("sent");
+    }
+  });
+});
+
+app.listen(3000, function(){
+console.log("Express Started on Port 3000");
 });
