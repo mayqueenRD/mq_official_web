5c8eb17fb806359fd45f8f1db0df2064654c6978
henrybi <henrybi@u.washington.edu>
2016-03-23 19:42:56 -0700

    email working!!!!!!!

--- js/mail.js
+++ js/mail.js
@@ -1,14 +1,28 @@
-(function(){
-var formInfo = {"party":"party"};
+
+console.log($("input[name='first_name']").val());
 
   $("form").on("submit", function(e){
-    // e.preventDefault();
-    console.log("sent");
+    var contactInfo = {name: $("input[name='first_name']").val() + " " + $("input[name='last_name']").val(),
+                    email: $("input[name='email']").val(),
+                    number: $("input[name='telephone']").val(),
+                    address: $("input[name='address']").val(),
+                    city: $("input[name='city']").val(),
+                    zipcode: $("input[name='zipcode']").val(),
+                    project: $("select[name='project']").val(),
+                    material: $("select[name='material']").val(),
+                    terrain: $("select[name='terrain']").val(),
+                    size: $("select[name='size']").val(),
+                    tree: $("input[name='tree']").val(),
+                    fence: $("input[name='fence']").val(),
+                    call: $("input[name='contact']").val(),
+                    comments: $("textarea[name='comments']").val()
+
+                };
+
+    e.preventDefault();
     $.ajax({
-      type: "POST",
-      url: "http://puget-fence-llc.herokuapp.com",
-      data: formInfo
+      type: "GET",
+      url: "http://0.0.0.0:3000/",
+      data: contactInfo
     });
   });
-
-})();
--- server.js
+++ server.js
@@ -1,34 +1,52 @@
 var express = require('express');
+var app = express();
 var nodemailer = require("nodemailer");
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
 
-var smtpConfig = {
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // use SSL
-    auth: {
-        user: "honghaobitest@gmail.com",
-        pass: "test1234567890"
-    }
-};
+app.get('/', function(req, res){
 
-var transporter = nodemailer.createTransport(smtpConfig);
+  var smtpConfig = {
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true, // use SSL
+      auth: {
+          user: "honghaobitest@gmail.com",
+          pass: "test1234567890"
+      }
+  };
 
-var mailOptions = {
-  from: '"Henry Bi 👥" <henrybidesign@gmail.com>',
-  to : "henrybi@uw.edu",
-  subject : "test",
-  text : "test",
-  html: '<b>Hello world 🐴</b>'
-};
+  var transporter = nodemailer.createTransport(smtpConfig);
+  console.log(req.query.tree);
+  var mailOptions = {
+    from: req.query.name + " 👥 " + req.query.email,
+    to: "henrybi@uw.edu",
+    subject: "fence pricing estimate",
+    text: "estimate",
+    html: "<h4>Hello Ben 🐴, You have a contact to make for an estimate</h4> <br>" +
+    "Client Name: " + req.query.name + "<br>"  +
+    "Email Address: " + req.query.email + "<br>" +
+    "Telephone Number: " + req.query.number + "<br>" +
+    "Address: " + req.query.address + "<br>" + req.query.city + " , " + req.query.zipcode + "<br>" +
+    "Project Type: " + req.query.project + "<br>" +
+    "Fencing Material: " + req.query.material + "<br>" +
+    "Terrain Description: " + req.query.terrain + "<br>" +
+    "Project Size: " + req.query.size + "<br>" +
+    "Existing Tree Removal: " + req.query.tree + "<br>" +
+    "Existing Fence Removal: " + req.query.fence + "<br>" +
+    "Best Time to Call: " + req.query.call + "<br>" +
+    "Other Comments: " + req.query.comments + "<br>"
+  };
 
-transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        return console.log(error);
-    }
-    console.log('Message sent: ' + info.response);
-});
+  transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+          return console.log(error);
+      }
+      console.log('Message sent: ' + info.response);
+  });
 
-app.post('/', function(req, res){
+  console.log(mailOptions);
+
   res.send('sent');
 });
 
